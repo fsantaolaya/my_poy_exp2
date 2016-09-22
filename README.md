5424dbe37d5c6f525aa2998b67438d9bf77a01a8
xe52360 <fernando.santa.contractor@bbva.com>
2016-09-22 18:43:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_poy_exp2
+
+A web app built using polymer.dart.
---
+++ web/index.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, xe52360. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:my_poy_exp2/main_app.dart';
+import 'package:polymer/polymer.dart';
+import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
+
+/// [MainApp] used!
+main() async {
+  await initPolymer();
+}
